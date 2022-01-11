8389a1bd68e0f40b26922dc870f64482481be0bb
Pragyan Tripathi <pragyan@vadelabs.com>
2022-01-11 16:46:32 +0530

    fix comment already refers to: cljs.core/comment warning

--- src/syn_antd/comment.cljs
+++ src/syn_antd/comment.cljs
@@ -1,6 +1,7 @@
 (ns syn-antd.comment
+  (:refer-clojure :exclude [comment])
   (:require
-    [reagent.core]
-    ["antd/es/comment" :default ant-comment]))
+   [reagent.core]
+   ["antd/es/comment" :default ant-comment]))
 
 (def comment (reagent.core/adapt-react-class ant-comment))
